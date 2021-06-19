3196c3066de4596341d46226ff0205a9a284cdb3
vde-guil <valentin.de.guillebon@gmail.com>
2021-06-19 14:51:18 +0200

    trying something else

--- back/app/database.js
+++ back/app/database.js
@@ -5,8 +5,9 @@ const sequelize = new Sequelize(process.env.PG_URL || process.env.DATABASE_URL, 
     define: {
         //permet le nom de champs en snake_case
         underscored: true,
+        dialect: "postgres",
+        ssl:true,
         dialectOptions: {
-            require: true,
             rejectUnauthorized: false
         }
     }

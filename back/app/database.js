e88a4600f252e7a289a8f4309fc229c438152b23
vde-guil <valentin.de.guillebon@gmail.com>
2021-06-19 14:57:37 +0200

    encore un

--- back/app/database.js
+++ back/app/database.js
@@ -1,16 +1,17 @@
 const { Sequelize } = require('sequelize');
 
 //On instancie un objet de la classe sequelize
-const sequelize = new Sequelize(process.env.PG_URL || process.env.DATABASE_URL, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     define: {
         //permet le nom de champs en snake_case
         underscored: true,
-        dialect: "postgres",
-        protocol: "postgres",
-        dialectOptions: {
-            ssl: true,
-            rejectUnauthorized: false
-        }
+
+    },
+    dialect: "postgres",
+    protocol: "postgres",
+    dialectOptions: {
+        ssl: true,
+        rejectUnauthorized: false
     }
 });
 
